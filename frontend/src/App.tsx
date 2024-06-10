099ad91c95beacfedf5bd0e1dcb14751d75c7d5d
robin singh <robinsingh1912@gmail.com>
2024-06-10 21:12:19 +0530

    style(frontend): :art: alias added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import BalanceSheet from '@/components/BalanceSheet';
+
 function App() {
   return (
     <>
       <h1 className='text-3xl font-bold underline text-center'>Hello world!</h1>
+      <BalanceSheet />
     </>
   );
 }
