b86474d10c13533490f9b1cdf7c171a843131455
robin singh <robinsingh1912@gmail.com>
2024-06-10 21:01:17 +0530

    feat(server): :closed_lock_with_key: added cors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import 'dotenv/config';
+import cors from 'cors'; // Import the cors middleware
+
 import balanceSheetRoutes from './routes/balanceSheet.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+const origin = process.env.ORIGIN || 'http://localhost:3000';
+app.use(cors({ origin, methods: ['GET'] }));
 
 app.use('/api/v1/BalanceSheet', balanceSheetRoutes);
 
