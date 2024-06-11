ee4fc474fb56ef1fc53a52f712931a284caecd32
robin singh <robinsingh1912@gmail.com>
2024-06-11 15:00:37 +0530

    feat(frontend): :label: interfaces and types added

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,38 @@
+export interface HeaderRow {
+  RowType: 'Header';
+  Cells: Record<'Value', string>[];
+}
+
+export interface Row {
+  Cells: {
+    Attributes: {
+      Id: string; // "account"
+      Value: string;
+    }[];
+    Value: string;
+  }[];
+  RowType: 'Row';
+}
+
+export interface SummaryRow {
+  Cells: {
+    Value: string;
+  }[];
+  RowType: 'SummaryRow';
+}
+
+export interface SectionRow {
+  RowType: 'Section';
+  Rows: (Row | SummaryRow)[];
+  Title: string;
+}
+export interface Report {
+  Fields: unknown[];
+  ReportDate: string;
+  ReportID: string;
+  ReportName: string;
+  ReportTitles: string[];
+  ReportType: 'BalanceSheet';
+  Rows: (HeaderRow | SectionRow)[];
+  UpdatedDateUTC: string;
+}
